ebcf3f56fec3465138d9130aa177f85e3cea9ef6
dabliuessi <wsmdm555@gmail.com>
2025-12-04 20:12:30 -0300

    Add navigation and update routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import DashboardUsuario from "./pages/DashboardUsuario";
@@ -8,17 +8,20 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
       <Route
-        path="/"
+        path="/usuario"
         element={
           <ProtectedRoute>
             <DashboardUsuario />
           </ProtectedRoute>
         }
       />
+
       <Route
         path="/tecnico"
         element={
