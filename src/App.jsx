53936e7e509cb930a829d65ad091642fbea00b4d
dabliuessi <wsmdm555@gmail.com>
2025-12-04 20:40:00 -0300

    Refactor routing for login and user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import DashboardUsuario from "./pages/DashboardUsuario";
@@ -8,20 +8,17 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Navigate to="/" replace />} />
-
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
       <Route
-        path="/usuario"
+        path="/"
         element={
           <ProtectedRoute>
             <DashboardUsuario />
           </ProtectedRoute>
         }
       />
-
       <Route
         path="/tecnico"
         element={
